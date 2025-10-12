7dc0072871b0638e4b9c89134a4375fa5777cd6e
René Theis <rene.tube992@gmail.com>
2025-10-12 09:35:47 +0200

    the logged-in user can retrieve his assigned tasks or those to be reviewed

--- tasks_app/api/urls.py
+++ tasks_app/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TasksViewSet
+from .views import TasksViewSet, TaskAssignedToCurrentUser, TaskreviewingCurrentUser
 
 router = DefaultRouter()
 router.register(r'', TasksViewSet, basename='tasks')
 
 urlpatterns = [
+    path('assigned-to-me/', TaskAssignedToCurrentUser.as_view(), name='tasks-assigned-to-me'),
+    path('reviewing/', TaskreviewingCurrentUser.as_view(), name='tasks-reviewing'),
     path('', include(router.urls)),
 ]
--- tasks_app/api/views.py
+++ tasks_app/api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
+
 from tasks_app.models import Task
 from .serializers import TaskListSerializer
 from .permissions import TaskPermission
@@ -8,3 +10,22 @@ class TasksViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskListSerializer
     permission_classes = [TaskPermission]
+
+
+class TaskAssignedToCurrentUser(generics.ListAPIView):
+    serializer_class = TaskListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        filtred_tasks = Task.objects.filter(assignee=user)
+        return filtred_tasks
+    
+class TaskreviewingCurrentUser(generics.ListAPIView):
+    serializer_class = TaskListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        filtred_tasks = Task.objects.filter(reviewer=user)
+        return filtred_tasks
